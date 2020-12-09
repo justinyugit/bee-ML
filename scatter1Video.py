b644175c5656be61eb38efd7c135574734bfb42e
Justin <47165787+justinyugit@users.noreply.github.com>
2020-12-09 10:50:13 -0500

    Added comments

--- scatter1Video.py
+++ scatter1Video.py
@@ -9,23 +9,39 @@ import sys
 import csv
 import os
 
+# load path of video
 name = sys.argv[1]
-txtName = name[27:37] + "--" + name[38:43]
+
+# used for later iterations of auto naming output txts
+# txtName = name[27:37] + "--" + name[38:43]
+
+#load video and YOLO weights into OpenCV
 video = cv2.VideoCapture(name)
 net = cv2.dnn.readNet("YOLO/yolov3_training_final.weights", "YOLO/yolov3_testing.cfg")
 layer_names=net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
+#Run inference by frame
 def YOLO(cap):
+    
+    #keep track of frames so x coord time can be tracked in output graph
     frames = 0
+    
+    # Each video is 20 minutes long @ 25fps so total 30000 frames
     while (frames < 30000):
         frames=frames+1
         ret, frame = cap.read()
+        
+        # set region of interest to speed up inference times (fit around the tube)
         img=frame[180:450, 0:800]
         height, width, channels = img.shape
+        
+        # begins detection process with loaded weights
         blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
         net.setInput(blob)
         outs = net.forward(output_layers)
+        
+        #iterate thru every detected object and check confidence to confirm detection. 
         for out in outs:
             for detection in out:
                 scores = detection[5:]
